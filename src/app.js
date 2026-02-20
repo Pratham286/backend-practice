5bfc6872f78ce40cf8c366bb00848992d50e9088
Pratham286 <22je0717@iitism.ac.in>
2026-02-20 16:26:57 +0530

    Adding middlewares and Utils

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors'
+import cookieParser from 'cookie-parser';
+
+
+const app = express();
+
+const corsURL = process.env.CORS_URL
+
+app.use(cors(
+    {
+        origin : corsURL,
+        credentials : true
+    }
+));
+
+app.use(express.json({limit : '16kb'}));
+
+app.use(express.urlencoded({extended : true, limit : '16kb'}));
+
+app.use(express.static("public"))
+
+app.use(cookieParser());
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -7,7 +7,19 @@ import { connectDB } from "./db/index.js";
 dotenv.config({path: './.env'})
 const app =express();
 // console.log(process.env.PORT)
-connectDB();
+
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+.then(() => {
+    app.listen(PORT, ()=>{
+        console.log(`Server Started at port : ${PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB Connection Failed, Error: ", err);
+})
+
 // IIFE
 // ;(async () => {
 //     try {
